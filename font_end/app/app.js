815a45d402f67fa33f308b893bc1665623f2deed
Lijingjing <lijingjing1008@gmail.com>
2015-11-16 23:28:32 +0800

    会员权益以及相应的注册方式前端界面

--- font_end/app/app.js
+++ font_end/app/app.js
@@ -2,10 +2,11 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.version',
-  'myApp.register'
+    'ngRoute',
+    'myApp.version',
+    'myApp.register',
+    'myApp.about'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/register'});
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/about'});
 }]);
