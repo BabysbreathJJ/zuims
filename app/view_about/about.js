071ce829792c62599f2c433c47c2f95c0f915a23
Lijingjing <lijingjing1008@gmail.com>
2015-11-18 20:48:40 +0800

    登录注册

--- app/view_about/about.js
+++ app/view_about/about.js
@@ -1,3 +1,37 @@
 /**
  * Created by Lijingjing on 15/11/12.
  */
+'use strict';
+
+
+angular.module('myApp.about', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/about', {
+            templateUrl: 'view_about/about.html',
+            controller: 'AboutCtrl'
+        });
+    }])
+    .controller('AboutCtrl', function ($scope, $location) {
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        var slides = $scope.slides = [];
+        $scope.addSlide = function (i) {
+            var newWidth = 600 + slides.length + 1;
+            slides.push({
+                image: 'images/' + i + '.jpg',
+                text: ['全国三免一折扣', '尊享VIP待遇', '智能推荐一键订'][slides.length % 3]
+            });
+        };
+        for (var i = 1; i < 4; i++) {
+            $scope.addSlide(i);
+        }
+
+
+        $scope.bePayMem = function () {
+            $location.path("/pay_register");
+        }
+
+        $scope.beFreeMem = function () {
+            $location.path("/experience_register");
+        }
+    });
