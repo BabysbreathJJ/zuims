8a2f0d51d08137f9cbfc20a4fff5b8655e2c35c1
Lijingjing <lijingjing1008@gmail.com>
2015-11-25 16:17:13 +0800

    完成了注册的交互以及增加了页面中的所有说明内容

--- app/view_about/about.js
+++ app/view_about/about.js
@@ -11,7 +11,14 @@ angular.module('myApp.about', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
             controller: 'AboutCtrl'
         });
     }])
-    .controller('AboutCtrl', function ($scope, $location) {
+    .controller('ModalCtrl', function ($scope, $uibModalInstance, items, title) {
+        $scope.items = items;
+        $scope.title = title;
+        $scope.ok = function () {
+            $uibModalInstance.dismiss('确定');
+        };
+    })
+    .controller('AboutCtrl', function ($scope, $location, $uibModal) {
         $scope.myInterval = 5000;
         $scope.noWrapSlides = false;
         var slides = $scope.slides = [];
@@ -29,9 +36,72 @@ angular.module('myApp.about', ['ngRoute', 'ngAnimate', 'ui.bootstrap'])
 
         $scope.bePayMem = function () {
             $location.path("/pay_register");
-        }
+        };
 
         $scope.beFreeMem = function () {
             $location.path("/experience_register");
-        }
+        };
+
+        $scope.detail1 = function () {
+            $scope.items = ['尊享会员在最美食全国范围内的合作商户消费享三免一优惠。', '三免一优惠指：三人同时在同一餐厅消费，可获其中一人免单。'];
+            $scope.title = "全国三免一折扣";
+            $scope.animationsEnabled = true;
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalCtrl',
+                size: 'sm',
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    },
+                    title: function () {
+                        return $scope.title;
+                    }
+                }
+            });
+        };
+
+        $scope.detail2 = function () {
+            $scope.items = ['尊享会员在最美食全国范围内的合作商户消费，可享受代客泊车、免费停车、预留专有餐位、VIP通道等服务。具体见商户详情。'];
+            $scope.title = "尊享VIP待遇";
+            $scope.animationsEnabled = true;
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalCtrl',
+                size: 'sm',
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    },
+                    title: function () {
+                        return $scope.title;
+                    }
+                }
+            });
+        };
+
+        $scope.detail3 = function () {
+            $scope.items = ['作为国内首家双语专业美食媒体，7年来，最美食从数百家星级酒店中甄选优秀餐厅作为推荐商户。','最美食根据会员的消费偏好和交通情况智能推荐适合每位会员的餐厅，一键完成预定。'];
+            $scope.title = "智能推荐一键订";
+            $scope.animationsEnabled = true;
+            var modalInstance = $uibModal.open({
+                animation: $scope.animationsEnabled,
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalCtrl',
+                size: 'sm',
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    },
+                    title: function () {
+                        return $scope.title;
+                    }
+                }
+            });
+        };
+
+
+
     });
