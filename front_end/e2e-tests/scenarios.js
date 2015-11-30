71887a881655bdf03d947619f8a03335b738860b
Lijingjing <lijingjing1008@gmail.com>
2015-11-30 22:39:32 +0800

    完成搜索与推荐的后台交互

--- front_end/e2e-tests/scenarios.js
+++ front_end/e2e-tests/scenarios.js
@@ -7,20 +7,20 @@ describe('my app', function() {
 
   it('should automatically redirect to /register when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/register");
+    expect(browser.getLocationAbsUrl()).toMatch("/about");
   });
 
 
   describe('view_register', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/register');
+      browser.get('#/experience_register');
     });
 
 
-    it('should render register when user navigates to /register', function() {
+    it('should render register when user navigates to /experience_register', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+        toMatch(/partial for view register/);
     });
 
   });
