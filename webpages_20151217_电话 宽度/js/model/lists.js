444c20c32de5bd4797ae6865a29120ba42a36c05
jim liu <jimliuxinghai@163.com>
2016-01-07 17:17:04 +0800

    list页面价格排序和更多

--- webpages_20151217_电话 宽度/js/model/lists.js
+++ webpages_20151217_电话 宽度/js/model/lists.js
@@ -3,25 +3,34 @@
  */
 //加载餐厅列表
 var resUrl = 'http://202.120.40.175:21100';
+var price = true;//排序时候用
+var hot = true;//排序时候用
 loadRes($.cookie('locateCity'));
+$("#current-city").html($.cookie('locateCity'));
 function loadRes(cname){
 
     $.ajax({
-        url : resUrl + "/restaurants/recommand/city",
+        url : resUrl + "/restaurants/city",
         type : "GET",
         data : {cname : cname},
         crossDomain: true,
         success : function(data){
-            var resInfo = loadDiv(data);
-            $(".resList").html("");
-            $(".resList").append(resInfo);
+            var newData = data;
+            var index = 10;
+            more(newData,index)
+            $("#more").click(function(){
+                index+=10;
+                more(newData,index)
+            });
+            //价格排序
+            sortPrice(newData,index);
+
         }
 
     })
 }
 //dom添加
 function loadDiv(data){
-    debugger;
     var resInfo = "";
     var smy = '<div class="pos-a font-white bg-orange transform45 smy">三免一</div>';
     for(var i = 0;i < data.length;i ++){
@@ -47,7 +56,7 @@ function loadDiv(data){
             '</div>'+
             '<p>'+
             '<span class="font-c-8f">每位 :</span>'+
-            '<span class="font-c-40">'+data[i].price+'元'+
+            '<span class="font-c-40">￥'+data[i].price+
             '</span>'+
             '</p>'+
             '<p>'+
@@ -55,15 +64,6 @@ function loadDiv(data){
             '<span class="font-c-40">'+data[i].address+
             '</span>'+
             '</p>'+
-            /*                             '<p>'+
-             '<span class="font-c-8f">电话 ：</span>'+
-             '<span>'+
-             '<a class="font-c-40" href="tel:'+tel[0]+'">'+tel[0]+
-             '</a>'+','+
-             '<a class="font-c-40" href="tel:'+tel[1]+'">'+tel[1]+
-             '</a>'+
-             '</span>'+
-             '</p>'+*/
             '<p>'+
             '<span class="font-c-8f">餐厅类别：</span>'+
             '<span class="font-c-40">'+data[i].restaurantType+
@@ -76,6 +76,59 @@ function loadDiv(data){
     }
     return resInfo;
 }
+//点击更多处理数据
+function more(data,index){
+    var newData = [];
+    for(var i = 0; i < index; i ++){
+        newData.push(data[i]);
+    }
+    var resInfo = loadDiv(newData);
+    $(".resList").html(resInfo);
+}
+//价格排序
+function sortPrice(newData,index){
+    $('.price').click(function(){
+
+        if(price){
+            //从下到上
+            $(this).find(".glyphicon-triangle-top").show();
+            $(this).find(".glyphicon-triangle-bottom").hide();
+            price = false;
+            newData = sort(newData,'small')
+
+        }
+        else{
+            //从上到下
+            $(this).find(".glyphicon-triangle-top").hide();
+            $(this).find(".glyphicon-triangle-bottom").show();
+            price = true;
+            newData = newData.reverse();
+            console.log(newData);
+
+        }
+        $('html,body').animate({scrollTop: '0px'}, 300);
+        more(newData,index)
+        $("#more").click(function(){
+            index+=10;
+            more(newData,index)
+        })
+    });
+}
+//热门点击
+$('.hot').click(function(){
+    if(hot){
+        //从下到上
+        $(this).find(".glyphicon-triangle-top").show();
+        $(this).find(".glyphicon-triangle-bottom").hide();
+        hot = false;
+    }
+    else{
+        //从上到下
+        $(this).find(".glyphicon-triangle-top").hide();
+        $(this).find(".glyphicon-triangle-bottom").show();
+        hot = true;
+    }
+});
 //搜素餐厅
 $(".search").click(function(){
     var resName = $(".searchText").val()
@@ -83,6 +136,7 @@ $(".search").click(function(){
 })
 
 function searchRes(resName){
+
     $.ajax({
         url : resUrl + "/restaurants/search",
         type : "GET",
@@ -104,7 +158,7 @@ function searchRes(resName){
     })
 }
 
-//选择城市
+
 //选择城市下拉列表触发事件
 $(".city-item").click(function(){
     $("#current-city").text($(this).text());
@@ -120,24 +174,21 @@ function menu(){
         $(this).find('span').addClass("font-c-f8");
     })
 }
-$(".hot").click(function(){
-    change($(this));
-});
-$(".price").click(function(){
-    change($(this));
-});
-var i = true;
-function change(obj){
-    if(i){
-        //从下到上
-        obj.find(".glyphicon-triangle-top").show();
-        obj.find(".glyphicon-triangle-bottom").hide();
-        i = false;
+
+
+//价格排序
+function sort(array){
+    var i = 0, len = array.length,
+        j, d;
+    for(; i<len; i++){
+        for(j=0; j<len; j++){
+                if(parseInt(array[i].price) < parseInt(array[j].price)){
+                    d = array[j];
+                    array[j] = array[i];
+                    array[i] = d;
+                }
+        }
     }
-    else{
-        //从上到下
-        obj.find(".glyphicon-triangle-top").hide();
-        obj.find(".glyphicon-triangle-bottom").show();
-        i = true;
-    }
+    return array;
+
 }
